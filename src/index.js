bd617401489312d01ad5ff199e41add089be5ba1
Joshua Byrd <phocks@gmail.com>
2018-07-23 11:28:31 +1000

    Starting out on an adventure

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const React = require("react");
+const { render } = require("react-dom");
+import { hashify } from "spanify";
+import * as d3 from "d3";
+
+hashify({ hashList: ["interactive"] }); // turn CoreMedia hashtags into divs
+
+d3.select("body").style("background-color", "darkcyan");
+
+d3.select("div.interactive").classed("u-full", true);
+
+const PROJECT_NAME = "interactive";
+const root = document.querySelector(".interactive");
+
+const init = () => {
+  const App = require("./components/App/App.js");
+  render(<App projectName={PROJECT_NAME} />, root);
+};
+
+init();
+
+if (module.hot) {
+  module.hot.accept("./components/App/App.js", () => {
+    try {
+      init();
+    } catch (err) {
+      const ErrorBox = require("./components/ErrorBox");
+      render(<ErrorBox error={err} />, root);
+    }
+  });
+}
+
+if (process.env.NODE_ENV === "development") {
+  console.debug(`[${PROJECT_NAME}] public path: ${__webpack_public_path__}`);
+}
